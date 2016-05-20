00893f9740c91f8e77ebbbcc0e694aa69138216d
aalberg <aralberg@gmail.com>
2016-05-19 19:10:41 -0700

    Cleanup part 1

--- util/test/spi_echo_test/uart_client.py
+++ util/test/spi_echo_test/uart_client.py
@@ -7,13 +7,10 @@ import sys
 import time
 
 def main():
-  #if len(sys.argv) < 2:
-  #  print("Not enough args")
-
   # Open serial port
   try:
     ser = serial.Serial(
-      port="COM3",#sys.argv[1],
+      port="COM3",
       baudrate=921600,#230400,
       parity=serial.PARITY_NONE,
       stopbits=serial.STOPBITS_ONE,
@@ -23,20 +20,25 @@ def main():
   except serial.SerialException as e:
     print(str(e), ser)
     return -1
-    
+
+  # mode = print characters to terminal in ascii or not.
+  # last = last character (used to verify no bytes were dropped).
+  # state_time and count = used to calculate data rate.
   mode = 0
   last = -1
   start_time = time.time()
   count = 0
   print "starting test"
+  
+  # Read bytes from the serial port.
   while 1:
     b = ser.read(1)
-    #print len(b), b, type(b), b[0], type(b[0])
     # Enable/disable ascii mode.
     if b[0] == '\xfe':
       mode = 1
     elif b[0] == '\xff':
       mode = 0
+    # Normal byte, verify it's the right byte.
     else:
       if mode == 0:
         cur = struct.unpack("<B", b[0])[0]
@@ -47,7 +49,7 @@ def main():
         #print binascii.hexlify(b)
       elif mode == 1:
         print(b)
-    
+
     count += 1
     if count % 1000 == 0:
       print "Average bitrate =", count / (time.time() - start_time), "Bps"
